55a6ad3b0c7fad74e08265aa8cb6776a953743c2
CadeParlato <cade.parloto@gmail.com>
2024-12-30 17:00:13 -0500

    Initial commit

---
+++ src/main/java/net/cade/goofytestmod/GoofyTestMod.java
@@ -0,0 +1,17 @@
+package net.cade.goofytestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoofyTestMod implements ModInitializer {
+	public static final String MOD_ID = "goofytestmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cade/goofytestmod/GoofyTestModClient.java
@@ -0,0 +1,10 @@
+package net.cade.goofytestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoofyTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

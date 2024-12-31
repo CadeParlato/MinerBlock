48d0c354f998de319bd08f21a95b71d5280d2f85
CadeParlato <cade.parloto@gmail.com>
2024-12-30 21:01:57 -0500

    Implemented basic goober

--- src/main/java/net/cade/goofytestmod/GoofyTestMod.java
+++ src/main/java/net/cade/goofytestmod/GoofyTestMod.java
@@ -1,5 +1,6 @@
 package net.cade.goofytestmod;
 
+import net.cade.goofytestmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class GoofyTestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.RegisterItems();
 	}
 }

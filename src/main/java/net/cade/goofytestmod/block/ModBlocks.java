ea9f0f1ae294bef76526ebebad92b8eb4eecf747
CadeParlato <cade.parloto@gmail.com>
2024-12-31 22:32:14 -0500

    Added Cool Block with working item

--- src/main/java/net/cade/goofytestmod/block/ModBlocks.java
+++ src/main/java/net/cade/goofytestmod/block/ModBlocks.java
@@ -8,19 +8,21 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
 
 public class ModBlocks {
 
-    public static final Block.Settings COOL_BLOCK_SETTINGS = Block.Settings.create().strength(2f);
+    public static final Block.Settings COOL_BLOCK_SETTINGS = Block.Settings.create().strength(2f)
+            .sounds(BlockSoundGroup.BONE);
     public static final Block COOL_BLOCK = registerBlock("cool_block", Block::new, COOL_BLOCK_SETTINGS);
 
     private static Block registerBlock(String name, Function<Block.Settings, Block> factory, Block.Settings settings) {
         final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(GoofyTestMod.MOD_ID, name));
         final Block block = Blocks.register(registryKey, factory, settings);
-        //Apparently the items register can just take a block :o
+        //Apparently the items register can just take a block :)
         Items.register(block);
         return block;
     }
